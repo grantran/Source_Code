50bc2a4181d3ca0ffec495c669a37d968de075ad
grantran <grant.tran@gmail.com>
2017-05-25 19:30:35 +0000

    first database migration, skeleton for tables and seed data

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,12 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, username: 'Alice'}),
+        knex('users').insert({id: 2, username: 'Bob'}),
+        knex('users').insert({id: 3, username: 'Charlie'}),
+        knex('users').insert({id: 4, username: 'Erica'}),
+        knex('users').insert({id: 5, username: 'Chris'}),
+        knex('users').insert({id: 6, username: 'Grant'})
       ]);
     });
 };
