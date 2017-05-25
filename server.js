4bbfea8d74903526e1d7b3bcb4e2208e419fe3dc
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-25 00:02:13 +0000

    added bootstrap sass and made some colums on main page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
-const bootstrap   = require('bootstrap');
+
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
