f7da80c066e87c99b7421732d403ed0a0b6865c7
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-27 00:14:23 +0000

    created get image from url function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -37,19 +37,73 @@ $(document).ready(function() {
     return div.innerHTML;
   }
 
+function getImageFromURL(resource){
 
+
+
+  $.get("https://cors-anywhere.herokuapp.com/" + escape(resource.url), function(data) {
+    // var meta = $(data).filter('meta[name="og:image"]').attr("content");
+    // console.log(meta)
+
+    // var logo = $(data).filter('img[id="logo"]').attr("src");
+    // var ogImage = $(data).filter('meta[property="og:image"]').attr("content");
+    // var twitterImage = $(data).filter('meta[name="twitter:image"]').attr("content");
+    // var schemaImage = $(data).filter('[itemprop="image"]').attr("src");
+
+
+    // var imageURL = ogImage || twitterImage || schemaImage || "http://placehold.it/400x400"
+
+    var imageURL = resource.url + "/" + $(data).filter("link[rel=apple-touch-icon]").attr('href');
+
+
+    $(`#resource-${resource.id}`).find(".resourceImage").attr("src", imageURL);
+    console.log("resource.url" + resource.url + "imageURL   " +  imageURL);
+
+
+
+});
+
+  // function proxify( request ) {
+  //   request.url = "http://www.inertie.org/ba-simple-proxy.php?mode=native&url=" + encodeURIComponent( request.url );
+  //   return request;
+  // }
+
+  //   // Create an instance of ImageResolver
+  //   // The proxy function is passed as an option
+
+
+  // var resolver = new ImageResolver( { requestPlugin : proxify } );
+  //   resolver.register(new ImageResolver.Opengraph());
+  //   resolver.register(new ImageResolver.FileExtension());
+  //   resolver.register(new ImageResolver.NineGag());
+  //   resolver.register(new ImageResolver.Instagram());
+  //   resolver.register(new ImageResolver.ImgurPage());
+
+  //   resolver.register(new ImageResolver.MimeType());
+  //   resolver.register(new ImageResolver.Flickr( '6a4f9b6d16c0eaced089c91a2e7e87ad' ));
+
+  //   resolver.register(new ImageResolver.Webpage());
+
+  // resolver.resolve(url, function(result){
+  //   if (result) {
+  //       console.log(result.image);
+  //   } else {
+  //       console.log( "No image found" );
+  //   }
+// });
+}
 
 
   // creates elements on the page for each tweet
   function createResourceElement(resource) {
     const html = `
-       <article class="thumbnail">
+       <article class="thumbnail" id="resource-${resource.id}">
             <div class="caption">
               <h3 href="${escape(resource.url)}">${escape(resource.title)}</h3>
               <p>caption</p>
               <p> Posted by </p>
             </div>
-            <a href="${escape(resource.url)}"><img src="http://placehold.it/350x150"></a>
+            <a href="${escape(resource.url)}"><img class="resourceImage" src=""></a>
             <div class = "footer">
               <p>TAG, TAG, TAG</p>
               <img href="" class="likeicon" src="/images/heart.png">
@@ -92,6 +146,7 @@ $(document).ready(function() {
     for (var i = 0; i < resources.length; i++) {
       var resource = resources[i];
       resourceContainer.prepend(createResourceElement(resource));
+      getImageFromURL(resource);
     }
   }
 
